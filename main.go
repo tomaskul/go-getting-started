cb56fdb6a6d803a20527b3a9cfe53e7cf238b0ea
tomaskul <>
2022-05-06 15:33:06 +0100

    Function fundamentals

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/tomaskul/go-getting-started/models"
 )
 
 func main() {
-	startWebServer()
+	port := 3000
+	retries := 3
+	_, err := startWebServer(port, retries)
+	fmt.Println(port, err)
 
-	u := models.User{
-		ID:        2,
-		FirstName: "Tricia",
-		LastName:  "McMillan",
-	}
+	/*
+		u := models.User{
+			ID:        2,
+			FirstName: "Tricia",
+			LastName:  "McMillan",
+		}
 
-	fmt.Println(u)
+		fmt.Println(u)*/
 }
 
-func startWebServer() {
+func startWebServer(port, numberOfRetries int) (int, error) {
 	fmt.Println("Starting web server...")
-	fmt.Println("Web server started.")
+	fmt.Println("   Startup retries: ", numberOfRetries)
+
+	return -1, errors.New("something went wrong")
+
+	//fmt.Println("Web server started. Listening on port: ", port)
+	//return port, nil
 }
