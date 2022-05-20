7f2fbae4cfb1b21ab1e090361052a5c02ee4a3c9
tomaskul <>
2022-05-20 19:45:00 +0100

    AddUser test, 30.4% coverage

--- models/user_test.go
+++ models/user_test.go
@@ -17,3 +17,12 @@ func Test_AddUser_ErrorOnSuppliedId(t *testing.T) {
 		t.Error("User added when it shouldn't have")
 	}
 }
+
+func Test_AddUser_Success(t *testing.T) {
+	var u = User{0, "Joe", "Bloggs"}
+	u2, err := AddUser(u)
+
+	if u2.ID == 0 && err != nil && len(GetUsers()) != 1 {
+		t.Error("User ID not set and/or error returned unexpectedly")
+	}
+}
