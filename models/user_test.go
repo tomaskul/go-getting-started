f1e10a1dbac4e0a7bc40f055781d0830ac70c208
tomaskul <>
2022-05-20 19:32:36 +0100

    Adding model tests 13% coverage `go test -cover`

--- models/user_test.go
+++ models/user_test.go
@@ -4,8 +4,16 @@ import (
 	"testing"
 )
 
-func Test_BasicSubtraction(t *testing.T) {
-	if 10-5 != 5 {
-		t.Error("Failed to subtract correctly")
+func Test_GetUsers_UninitializedShouldBeNil(t *testing.T) {
+	if GetUsers() != nil {
+		t.Error("Found users when none should be present")
+	}
+}
+
+func Test_AddUser_ErrorOnSuppliedId(t *testing.T) {
+	var u = User{7, "FirstName", "Lastname"}
+	_, err := AddUser(u)
+	if err == nil {
+		t.Error("User added when it shouldn't have")
 	}
 }
