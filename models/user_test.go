aeaea0d67479a17066287e83d805f65bc6ed843b
tomaskul <>
2022-05-21 12:15:43 +0100

    Tests are atomic??

--- models/user_test.go
+++ models/user_test.go
@@ -10,19 +10,58 @@ func Test_GetUsers_UninitializedShouldBeNil(t *testing.T) {
 	}
 }
 
-func Test_AddUser_ErrorOnSuppliedId(t *testing.T) {
-	var u = User{7, "FirstName", "Lastname"}
-	_, err := AddUser(u)
+func Test_AddUser_SuppliedIdShouldError(t *testing.T) {
+	_, err := AddUser(User{7, "FirstName", "Lastname"})
 	if err == nil {
 		t.Error("User added when it shouldn't have")
 	}
 }
 
-func Test_AddUser_Success(t *testing.T) {
-	var u = User{0, "Joe", "Bloggs"}
-	u2, err := AddUser(u)
+func Test_AddUser_ValidInputShouldSucceed(t *testing.T) {
+	u, err := AddUser(User{0, "Joe", "Bloggs"})
 
-	if u2.ID == 0 && err != nil && len(GetUsers()) != 1 {
+	if u.ID == 0 && err != nil && len(GetUsers()) != 1 {
 		t.Error("User ID not set and/or error returned unexpectedly")
+	}
+}
+
+func Test_GetByUserId_UnknownIdShouldBeNil(t *testing.T) {
+	// Arrange.
+	var inputUsers = [...]*User{{0, "Joe", "Bloggs"},
+		{0, "Tim", "Bloggs"},
+		{0, "Alice", "Bloggs"}}
+
+	for _, u := range inputUsers {
+		AddUser(*u)
+	}
+
+	// Act.
+	u, err := GetUserByID(7)
+
+	// Assert.
+	if (u != User{} || err == nil) {
+		t.Error("Returned a user for unknown ID")
+	}
+}
+
+// Fails because tests aren't being ran atomically.
+func Test_GetUserById_ValidIdShouldGetUser(t *testing.T) {
+	// Arrange.
+	var inputUsers = [...]*User{{0, "Joe", "Bloggs"},
+		{0, "Tim", "Bloggs"},
+		{0, "Alice", "Bloggs"}}
+
+	for _, iu := range inputUsers {
+		AddUser(*iu)
+	}
+
+	targetId := 2
+
+	// Act.
+	u, err := GetUserByID(targetId)
+
+	// Assert.
+	if u.ID != targetId || err != nil || u.FirstName != "Tim" {
+		t.Errorf("Returned unknown result, u.FirstName: %s", u.FirstName)
 	}
 }
